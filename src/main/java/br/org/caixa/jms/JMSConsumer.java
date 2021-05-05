cc10983f5ab2f061474366d8d60903fc5430c3d1
Bruno Santos <brsantos87@gmail.com>
2021-05-05 15:33:10 -0300

    Implementacao conectionFactory mq

--- src/main/java/br/org/caixa/jms/JMSConsumer.java
+++ src/main/java/br/org/caixa/jms/JMSConsumer.java
@@ -1,66 +1,59 @@
 package br.org.caixa.jms;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 
+import org.apache.log4j.Logger;
+
+import br.org.caixa.jms.factory.ConnectionFactoryMQ;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
-
 
 @ApplicationScoped
 public class JMSConsumer implements Runnable {
 
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+	private static final String DEV_QUEUE_1 = "DEV.QUEUE.1";
 
-    void onStart(@Observes StartupEvent ev) {
-        scheduler.scheduleWithFixedDelay(this, 0L, 1L, TimeUnit.SECONDS);
-    }
+	private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-    void onStop(@Observes ShutdownEvent ev) {
-        scheduler.shutdown();
-    }
+	private final Logger logger = Logger.getLogger(JMSConsumer.class);
 
-    @Override
-    public void run() {
-    	
-//    	QueueConnection queueConnection = null;
-//		QueueSession queueSession = null;
-//		QueueSender queueSender = null;
-//		QueueReceiver queueReceiver = null;
-//
-//		QueueConnectionFactory queueConnectionFactory = getConnection();
-//		queueConnection = queueConnectionFactory.createQueueConnection();
-//
-//		Queue toQueue = (Queue) lookup(propriedadesMQ.getFilaRequisicao());
-//
-//		queueSession = queueConnection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
-//		queueConnection.start();
-//		queueSender = queueSession.createSender(toQueue);
-//
-//		TextMessage toMessage = queueSession.createTextMessage();
-//		toMessage.setStringProperty("JMS_IBM_Character_Set", "819");
-//		toMessage.setStringProperty("JMS_IBM_Format", "MQSTR");
-//		toMessage.setText(mensagem);
-//
-//		queueSender.send(toMessage);
-//    	
-//        try (JMSContext context = connectionFactory.createContext(Session.AUTO_ACKNOWLEDGE)) {
-//            javax.jms.JMSConsumer consumer = context.createConsumer(context.createQueue("DEV.QUEUE.1"));
-//            while (true) {
-//                Message message = consumer.receive();
-//                if (message == null) {
-//                    return;
-//                }
-//                System.out.println(message);
-//            }
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-    }
+	void onStart(@Observes StartupEvent ev) {
+		scheduler.scheduleWithFixedDelay(this, 0L, 1L, TimeUnit.SECONDS);
+	}
 
+	void onStop(@Observes ShutdownEvent ev) {
+		scheduler.shutdown();
+	}
 
+	@Override
+	public void run() {
+		logger.info("Iniciando listener " + DEV_QUEUE_1);
+		ConnectionFactoryMQ conectioFactory = new ConnectionFactoryMQ();
+		try (Connection connection = conectioFactory.getConnection().createConnection();
+				Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+				MessageConsumer consumer = session.createConsumer(session.createQueue(DEV_QUEUE_1))) {
+			while (true) {
+				connection.start();
+				TextMessage textMsg = (TextMessage) consumer.receive();
+				if (textMsg.getText() == null) {
+					return;
+				}
+				logger.info(String.format("Received JMSCorrelationID: %s message: %s",textMsg.getJMSCorrelationID() , textMsg.getText()));
+			}
+		} catch (JMSException e) {
+			logger.error(e);
+		}
+
+	}
 
 }
---
+++ src/main/java/br/org/caixa/jms/factory/ConnectionFactoryMQ.java
@@ -0,0 +1,38 @@
+package br.org.caixa.jms.factory;
+
+import java.util.ResourceBundle;
+
+import javax.jms.JMSException;
+
+import org.apache.log4j.Logger;
+
+import com.ibm.mq.jms.MQConnectionFactory;
+
+public class ConnectionFactoryMQ {
+
+	private final Logger logger = Logger.getLogger(ConnectionFactoryMQ.class);
+
+	private static final ResourceBundle properties = ResourceBundle.getBundle("mq");
+
+	private static ResourceBundle getProperties() {
+		return properties;
+	}
+
+	public MQConnectionFactory getConnection() {
+		MQConnectionFactory cf = new MQConnectionFactory();
+
+		logger.info("Obtendo conexao com fila MQ");
+		try {
+			cf.setHostName(getProperties().getString("mq.hostname"));
+			cf.setChannel(getProperties().getString("mq.channel"));
+			cf.setQueueManager(getProperties().getString("mq.queueManager"));
+			cf.setTransportType(com.ibm.msg.client.wmq.common.CommonConstants.WMQ_CM_CLIENT);
+			cf.setPort(Integer.parseInt(getProperties().getString("mq.port")));
+		} catch (NumberFormatException | JMSException e) {
+			logger.error("Erro ao conectar ao obter conexao MQ");
+		}
+		return cf;
+	}
+
+
+}
